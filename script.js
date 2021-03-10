37a8550387049529e152c3a60a2fbc0ac0823623
Leandro Reis <lleandrofr@gmail.com>
2021-03-10 15:52:45 -0300

    Muda o bg do item selecionado para cinza e somente um item pode ser selecionado

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-
-
 // ADICIONA O TEXTO DE INPUT NA LISTA DE TAREFAS.
 function addTask() {
 
   let inputText = document.querySelector('#texto-tarefa');
   let taskList = document.querySelector('#lista-tarefas');
 
+
   let newElem = document.createElement('li');
   newElem.innerText = inputText.value;
   newElem.className = 'list-item';
-
   taskList.appendChild(newElem);
 
   inputText.value = '';
+
+  newElem.addEventListener('click', changeClass);
+
+  console.log(newElem.innerHTML);
+
 }
 
 let button = document.getElementById('criar-tarefa');
 button.addEventListener('click', addTask);
+
+
+
+function changeClass() {
+  let currentSelected = document.querySelector('.selected');
+  if (currentSelected !== null) {
+    currentSelected.classList.remove('selected')
+  }
+  this.className = 'selected';
+}
